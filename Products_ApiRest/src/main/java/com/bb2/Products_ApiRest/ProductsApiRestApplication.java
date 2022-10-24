1743d169a218154df8cf8b7484686bc3fd85c835
ABRAHAM QUINTANA MICO <abraham.quintana@ikeasi.com>
2022-10-24 13:16:53 +0100

    Initial commit

---
+++ Products_ApiRest/src/main/java/com/bb2/Products_ApiRest/ProductsApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.bb2.Products_ApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiRestApplication.class, args);
+	}
+
+}
---
+++ Products_ApiRest/src/test/java/com/bb2/Products_ApiRest/ProductsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb2.Products_ApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
